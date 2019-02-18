613774585a44a0106d81e35ec5d125924042bd1b
Rahmani Saygın <rahmanisaygin@gmail.com>
2019-02-18 17:15:09 +0300

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum=0,i;
+    for (i=1; i<1000; i++)
+        if (i%3==0 || i%5==0)
+        {
+            sum += i;
+            printf("%d ", i); //Değerleri görmek için ekrana yazdıralım.
+        }
+
+    printf("\n>> %d ",sum);
+}
